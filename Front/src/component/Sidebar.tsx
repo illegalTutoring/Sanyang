a84ff2eac2f206bd67b9fa9ce424d8449f60f24a
남찬현 <chanhyun0821@gmail.com>
2024-04-12 15:41:06 +0900

    feat(sidebar):사이드바에 링크 추가

--- Front/src/component/Sidebar.tsx
+++ Front/src/component/Sidebar.tsx
@@ -1,7 +1,30 @@
 import styles from './Sidebar.module.css'
+import Link from 'next/link'
 
 const Sidebar: React.FC = () => {
-    return <aside className={styles.sidebar}>메뉴 영역</aside>
+    return (
+        <aside className={styles.sidebar}>
+            <nav>
+                <ul>
+                    <li>
+                        <Link href="/">Home</Link>
+                    </li>
+                    <li>
+                        <Link href="/outsourcing">외주</Link>
+                    </li>
+                    <li>
+                        <Link href="/gallery">개인</Link>
+                    </li>
+                    <li>
+                        <Link href="/support">서포트</Link>
+                    </li>
+                    <li>
+                        <Link href="/inquire">문의</Link>
+                    </li>
+                </ul>
+            </nav>
+        </aside>
+    )
 }
 
 export default Sidebar
