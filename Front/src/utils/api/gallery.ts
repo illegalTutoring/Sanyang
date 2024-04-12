ca5588920e6272d2ae94cc0e41ba903461bed77d
오철수 <target511@kakao.com>
2024-04-12 15:46:05 +0900

    feat(gallery): gallery API 호출함수 추가

---
+++ Front/src/utils/api/gallery.ts
@@ -0,0 +1,85 @@
+import axios from 'axios'
+
+const server_url = process.env.SERVER_URL
+
+export async function getGalleryList() {
+    /**
+     * 갤러리 이미지 정보 리스트를 반환한다.
+     *
+     * @returns galleryResponseGetDto 객체
+     *
+     * @beta
+     */
+
+    // const response = await axios({
+    //     method: 'GET',
+    //     url: `${server_url}/gallery`
+    // })
+    // return response.data
+
+    // START - DUMMY DATA
+    return {
+        message: '갤러리 목록 불러오기 성공',
+        galleryList: [
+            {
+                userId: 'sanyang',
+                title: '피카피카 우는 토끼',
+                upload_date: '2024-04-12 12:34:56',
+                image: {
+                    s3Path: '/s3/.../',
+                },
+            },
+            {
+                userId: 'sanyang',
+                title: '바보바보 우는 예원',
+                upload_date: '2024-05-12 12:34:56',
+                image: {
+                    s3Path: '/s3/.../',
+                },
+            },
+        ],
+    }
+    // END - DUMMY DATA
+}
+
+export async function getGalleryListByTag(tagList: Array<string>) {
+    /**
+     * 태그에 해당하는 갤러리 이미지 정보 리스트를 반환한다.
+     *
+     * @param tagList - tag들이 담긴 Array
+     * @returns galleryResponseGetDto 객체
+     *
+     * @beta
+     */
+
+    // TODO: 태그가 비어있다면 getGalleryList를 호출하도록 설정
+
+    // let tagString: string = '';
+    // for (let tag in tagList) {
+    //     tagString += `${tag},`
+    // }
+    // // 마지막 comma 제거
+    // tagString = tagString.substring(0, tagString.length - 2)
+
+    // const response = await axios({
+    //     method: 'GET',
+    //     url: `${server_url}/gallery/${tagString}`
+    // })
+    // return response.data
+
+    // START - DUMMY DATA
+    return {
+        message: '태그 검색 결과',
+        galleryList: [
+            {
+                userId: 'sanyang',
+                title: '피카피카 우는 토끼',
+                upload_date: '2024-04-12 12:34:56',
+                image: {
+                    s3Path: '/s3/.../',
+                },
+            },
+        ],
+    }
+    // END - DUMMY DATA
+}
