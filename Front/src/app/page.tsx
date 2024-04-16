0a7a502f39784151db8090558e16f93b09ef722c
남찬현 <chanhyun0821@gmail.com>
2024-04-16 09:12:32 +0900

    feat(home): 배너 컴포넌트 추가

--- Front/src/app/page.tsx
+++ Front/src/app/page.tsx
@@ -1,6 +1,20 @@
-export default function GalleryPage() {
+'use client'
+
+import Banner from '@/component/Banner'
+
+const HomePage = () => {
     return (
         <>
+            <Banner
+                images={[
+                    'https://pbs.twimg.com/media/Feng68VakAAKD6u?format=jpg&name=large',
+                    'https://pbs.twimg.com/media/Feng68WaEAIQvfS?format=jpg&name=large',
+                    'https://pbs.twimg.com/media/Feng68SagAAfkW3?format=jpg&name=4096x4096',
+                ]}
+                interval={3000}
+                width="100%"
+                height="400px"
+            />
             <h1>메인 페이지</h1>
             <h1>메인 페이지</h1>
             <h1>메인 페이지</h1>
@@ -40,3 +54,5 @@ export default function GalleryPage() {
         </>
     )
 }
+
+export default HomePage
---
+++ Front/src/component/Banner.tsx
@@ -0,0 +1,37 @@
+import React, { useState, useEffect } from 'react'
+
+interface BannerProps {
+    images: string[]
+    interval: number
+    width?: string
+    height?: string
+}
+
+const Banner: React.FC<BannerProps> = ({
+    images,
+    interval,
+    width = '100%',
+    height = '300px',
+}) => {
+    const [currentIdx, setCurrentIdx] = useState(0)
+
+    useEffect(() => {
+        const timer = setInterval(() => {
+            setCurrentIdx((prevIdx) => (prevIdx + 1) % images.length)
+        }, interval)
+
+        return () => clearInterval(timer)
+    }, [images.length, interval])
+
+    return (
+        <div style={{ width, height, overflow: 'hidden' }}>
+            <img
+                src={images[currentIdx]}
+                alt="Banner Image"
+                style={{ width: '100%', height: '100%', objectFit: 'cover' }}
+            />
+        </div>
+    )
+}
+
+export default Banner
