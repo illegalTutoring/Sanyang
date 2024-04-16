171e8eda1cb7b4536c896d23edfa340bc0f459b5
오철수 <target511@kakao.com>
2024-04-16 13:53:03 +0900

    feat(outsourcing): 외주 API 더미데이터 생성 및 예외 처리

--- Front/src/utils/api/outsourcing.ts
+++ Front/src/utils/api/outsourcing.ts
@@ -1,4 +1,5 @@
 import axios, { AxiosResponse } from 'axios'
+import { axiosRequestHandler } from './error'
 
 const SERVER_URL = process.env.SERVER_URL
 
@@ -6,24 +7,43 @@ export async function getOutsourcingList(year: number, month: number) {
     /**
      * 월별 외주 리스트를 반환한다.
      *
+     * @beta
+     * @todo
+     * 테스트
+     *
      * @param year - 조회하고자 하는 연도
      * @param month - 조회하고자 하는 월
-     * @returns outsourcingResponseGetDto 객체
-     *
-     * @beta
+     * @returns
+     * {
+     *      message: string,
+     *      outsourcingList: [
+     *          {
+     *              outsourcingId: int,
+     *              userId: string,
+     *              client: string,
+     *              title: string,
+     *              startDate: string,
+     *              endDate: string,
+     *          },
+     *          ...
+     *      ]
+     * }
      */
 
-    // const response: AxiosResponse<JSON> = await axios({
-    //     method: 'GET',
-    //     url: `${SERVER_URL}/outsourcing/${year}/${month}`,
-    // })
-    // return response.data
+    // return axiosRequestHandler(async (year: number, month: number) => {
+    //     const response: AxiosResponse<any, any> = await axios({
+    //         method: 'GET',
+    //         url: `${SERVER_URL}/outsourcing/${year}/${month}`
+    //     })
+    //     return response.data
+    // }, [year, month])
 
     // START - DUMMY DATA
     return {
         message: `${year}년 ${month}월 외주 목록입니다.`,
         outsourcingInfo: [
             {
+                outsourcingId: 2,
                 userId: 'sanyang',
                 client: 'D&F',
                 title: 'D&F 신규 캐릭터 일러스트 작업',
@@ -31,6 +51,7 @@ export async function getOutsourcingList(year: number, month: number) {
                 endDate: '2024-04-30',
             },
             {
+                outsourcingId: 3,
                 userId: 'sanyang',
                 client: 'D&F',
                 title: 'D&F 신규 업데이트 일러스트 작업',
@@ -49,37 +70,75 @@ export async function insertOutsourcingPassword(
     /**
      * 입력한 비밀번호가 올바르다면, 외주 상세 정보를 반환한다.
      *
+     * @beta
+     * @todo
+     * 테스트
+     *
      * @param outsourcing_id - 조회하고자 하는 외주의 id
      * @param password - 사용자가 입력한 비밀번호
-     * @returns outsourcingDetailResponsePostDto 객체
-     *
-     * @beta
+     * @returns
+     * {
+     *      message: string,
+     *      outsourcingInfo: [
+     *          {
+     *              outsourcingId: int,
+     *              userId: string,
+     *              client: string,
+     *              title: string,
+     *              content: string,
+     *              images: [
+     *                  {
+     *                      imagePath: string
+     *                  },
+     *                  ...
+     *              ]
+     *          }
+     *      ]
+     * }
      */
 
-    // const response: AxiosResponse<JSON> = await axios({
-    //     method: 'POST',
-    //     url: `${SERVER_URL}/outsourcing/${outsourcing_id}`,
-    //     data: {
-    //         password,
-    //     },
-    // })
-    // return response.data
+    // return axiosRequestHandler(async (outsourcing_id: string, password: string) => {
+    //     const response: AxiosResponse<any, any> = await axios({
+    //         method: 'POST',
+    //         url: `${SERVER_URL}/outsourcing/${outsourcing_id}`,
+    //         data: {
+    //             password,
+    //         },
+    //     })
+    //     return response.data
+    // }, [outsourcing_id, password])
 
     // START - DUMMY DATA
     return {
         message: '외주 비밀번호 일치',
         outsourcingInfo: [
             {
+                outsourcingId: 2,
                 userId: 'sanyang',
                 client: 'D&F',
                 title: 'D&F 신규 캐릭터 일러스트 작업',
                 content: '작업 내용 (Contents)',
                 image: [
                     {
-                        s3Path: '/s3/...',
+                        imagePath: '/s3/...',
                     },
                     {
-                        s3Path: '/s3/...',
+                        imagePath: '/s3/...',
+                    },
+                ],
+            },
+            {
+                outsourcingId: 3,
+                userId: 'sanyang',
+                client: 'D&F',
+                title: 'D&F 신규 업데이트 일러스트 작업',
+                content: '작업 내용 (Contents)',
+                image: [
+                    {
+                        imagePath: '/s3/...',
+                    },
+                    {
+                        imagePath: '/s3/...',
                     },
                 ],
             },
@@ -90,25 +149,51 @@ export async function insertOutsourcingPassword(
 
 export async function searchOutsourcingByName(keyword: string) {
     /**
-     * 입력한 비밀번호가 올바르다면, 외주 상세 정보를 반환한다.
-     *
-     * @param keyword - 검색하고자 하는 외주의 이름
-     * @returns outsourcingSearchResponseGetDto 객체
+     * 외주 이름으로 외주 검색
      *
      * @beta
+     * @todo
+     * 테스트
+     *
+     * @param keyword - 검색하고자 하는 외주의 이름
+     * @returns
+     * {
+     *      message: string,
+     *      outsourcingSearchList: [
+     *          {
+     *              outsourcingId: int,
+     *              userId: string,
+     *              client: string,
+     *              title: string,
+     *              startDate: string,
+     *              endDate: string,
+     *          },
+     *          {
+     *              outsourcingId: int,
+     *              userId: string,
+     *              client: string,
+     *              title: string,
+     *              startDate: string,
+     *              endDate: string,
+     *          }
+     *      ],
+     *  }
      */
 
-    // const response: AxiosResponse<JSON> = await axios({
-    //     method: 'GET',
-    //     url: `${SERVER_URL}/outsourcing/search/${keyword}`,
-    // })
-    // return response.data
+    // return axiosRequestHandler(async (keyword: string) => {
+    //     const response: AxiosResponse<any, any> = await axios({
+    //         method: 'GET',
+    //         url: `${SERVER_URL}/outsourcing/search/${keyword}`
+    //     })
+    //     return response.data
+    // }, [keyword])
 
     // START - DUMMY DATA
     return {
         message: "'D&F' 로 검색된 결과입니다.",
         outsourcingSearchList: [
             {
+                outsourcingId: 2,
                 userId: 'sanyang',
                 client: 'D&F',
                 title: 'D&F 신규 캐릭터 일러스트 작업',
@@ -116,6 +201,7 @@ export async function searchOutsourcingByName(keyword: string) {
                 endDate: '2024-04-30',
             },
             {
+                outsourcingId: 3,
                 userId: 'sanyang',
                 client: 'D&F',
                 title: 'D&F 신규 업데이트 일러스트 작업',
