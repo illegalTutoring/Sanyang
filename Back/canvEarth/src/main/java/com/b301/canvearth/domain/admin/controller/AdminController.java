98ce589d2a5b1170bf6af1e5500b3bb057f4e09d
김태희 <apples5060@naver.com>
2024-04-16 15:51:25 +0900

    feat(security): SecurityConfig 설정 및 기본 Controller 설정
    1. SecurityConfig의 EnableWebSecurity설정 및 Bean(BCryptPasswordEncoder, SecurityFilterChain) 설정
    2. User, Admin Controller 테스트 코드 생성

--- Back/canvEarth/src/main/java/com/b301/canvearth/domain/admin/controller/AdminController.java
+++ Back/canvEarth/src/main/java/com/b301/canvearth/domain/admin/controller/AdminController.java
@@ -1,4 +1,14 @@
 package com.b301.canvearth.domain.admin.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class AdminController {
+
+    @GetMapping("/admin")
+    public String adminP() {
+
+        return "admin Controller";
+    }
 }
--- Back/canvEarth/src/main/java/com/b301/canvearth/domain/user/controller/UserController.java
+++ Back/canvEarth/src/main/java/com/b301/canvearth/domain/user/controller/UserController.java
@@ -1,4 +1,18 @@
 package com.b301.canvearth.domain.user.controller;
 
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/user")
 public class UserController {
+
+    @GetMapping("/test")
+    public String testP(){
+        return "test";
+    }
+
+    @PostMapping("/test")
+    public String testP2(){
+        return "test2";
+    }
 }
