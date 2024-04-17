82c499deb86a18a32858255af86c3b5f8fe087c5
김태희 <apples5060@naver.com>
2024-04-17 14:31:45 +0900

    feat(security): User login 처리 로직 생성
    1. UserDetailsService 상속받아 커스텀(CustomUserDetailService)
    2. UserDetails 상속받아 커스텀(CustomUserDetails)
    3. 커스텀 한 클래스를 통해 회원 조회하고 데이터를 관리
    4. UsernamePasswordAuthenticationFilter를 커스텀하여 위 과정을 진행

--- Back/canvEarth/src/main/java/com/b301/canvearth/global/config/SecurityConfig.java
+++ Back/canvEarth/src/main/java/com/b301/canvearth/global/config/SecurityConfig.java
@@ -1,17 +1,34 @@
 package com.b301.canvearth.global.config;
 
+import com.b301.canvearth.global.jwt.LoginFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    private final AuthenticationConfiguration authenticationConfiguration;
+
+    public SecurityConfig(AuthenticationConfiguration authenticationConfiguration) {
+
+        this.authenticationConfiguration = authenticationConfiguration;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+
+        return configuration.getAuthenticationManager();
+    }
 
     @Bean
     public BCryptPasswordEncoder bCryptPasswordEncoder() {
@@ -37,9 +54,14 @@ public class SecurityConfig {
         //경로별 인가 작업
         http
                 .authorizeHttpRequests((auth) -> auth
-                        .requestMatchers("/login", "/", "/join","/api/user/test").permitAll()
+                        .requestMatchers("/login", "/", "/api/user/*").permitAll()
                         .requestMatchers("/admin").hasRole("ADMIN")
                         .anyRequest().authenticated());
+
+        //필터 추가 LoginFilter()는 인자를 받음 (AuthenticationManager() 메소드에 authenticationConfiguration 객체를 넣어야 함) 따라서 등록 필요
+        http
+                .addFilterAt(new LoginFilter(authenticationManager(authenticationConfiguration)), UsernamePasswordAuthenticationFilter.class);
+
 
         //세션 설정
         http
