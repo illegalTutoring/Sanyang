29123df8b44f3a48c999be57d63aaa5a162b933b
임혜령 <hyegpfud@gmail.com>
2024-04-23 16:07:22 +0900

    fix(calendar): entity 수정에 따른 응답 dto 수정

--- Back/canvEarth/src/main/java/com/b301/canvearth/domain/calendar/dto/CalendarResponseGetDto.java
+++ Back/canvEarth/src/main/java/com/b301/canvearth/domain/calendar/dto/CalendarResponseGetDto.java
@@ -13,29 +13,16 @@ public class CalendarResponseGetDto {
     private final int calendarId;
     private final String userId;
     private final String title;
-    private final String company;
     private final String startDate;
     private final String endDate;
-    private final Date uploadDate;
-    private final List<String> tag;
-    private final String originalPath;
-    private final String thumbnailPath;
-    private final String watermarkPath;
 
     @Builder
     public CalendarResponseGetDto(int calendarId, String userId, String title,
-                                  String company, String startDate, String endDate, Date uploadDate,
-                                  List<String> tag, String originalPath, String thumbnailPath, String watermarkPath) {
+                                  String startDate, String endDate) {
         this.calendarId = calendarId;
         this.userId = userId;
         this.title = title;
-        this.company = company;
         this.startDate = startDate;
         this.endDate = endDate;
-        this.uploadDate = uploadDate;
-        this.tag = tag;
-        this.originalPath = originalPath;
-        this.thumbnailPath = thumbnailPath;
-        this.watermarkPath = watermarkPath;
     }
 }
