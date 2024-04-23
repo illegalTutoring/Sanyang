0333164ac16c20154bc9455b6aa1ad547ae40060
오철수 <target511@kakao.com>
2024-04-23 14:23:28 +0900

    feat(admin): admin-gallery API 호출 함수 추가

--- Front/src/utils/api/admin.ts
+++ Front/src/utils/api/admin.ts
@@ -6,8 +6,6 @@ const SERVER_URL = process.env.SERVER_URL
 // TODO: redux에서 값을 가져오도록 수정할 것.
 let accessToken: string = 'TEST_ACCESS_TOKEN_IT_MUST_BE_CHANGED'
 
-// TODO: 갤러리 등록, 갤러리 수정, 갤러리 삭제
-
 export function registWork(data: JSON, image: File) {
     /**
      * 외주 등록
@@ -110,5 +108,110 @@ export function deleteWork(workId: number) {
             return { message: response.data.message }
         },
         [workId],
+    )
+}
+
+export function registGallery(data: JSON, image: File) {
+    /**
+     * 갤러리 등록
+     *
+     * @param data - 갤러리 정보
+     * @param image - 이미지
+     * @returns 서버 응답 메시지
+     *
+     * @beta
+     * @todo
+     * 테스트
+     *
+     */
+
+    return axiosRequestHandler(
+        async (data, image) => {
+            const blobData = new Blob([JSON.stringify(data)], {
+                type: 'application/json',
+            })
+            const formData = new FormData()
+            formData.append('data', blobData)
+            formData.append('image', image)
+
+            const response: AxiosResponse<any, any> = await axios({
+                method: 'POST',
+                url: `${SERVER_URL}/admin/gallery`,
+                data: formData,
+                headers: {
+                    'Content-Type': 'multipart/form-data',
+                    Authorization: accessToken,
+                },
+            })
+            return { message: response.data.message }
+        },
+        [data, image],
+    )
+}
+
+export function modifyGallery(data: JSON, image: File | null) {
+    /**
+     * 외주 수정
+     *
+     * @param data - 갤러리 정보
+     * @param image - 이미지 (수정되지 않으면 null)
+     * @returns 서버 응답 메시지
+     *
+     * @beta
+     * @todo
+     * 테스트
+     *
+     */
+
+    return axiosRequestHandler(
+        async (data, image) => {
+            const blobData = new Blob([JSON.stringify(data)], {
+                type: 'application/json',
+            })
+            const formData = new FormData()
+            formData.append('data', blobData)
+
+            if (image) formData.append('image', image)
+
+            const response: AxiosResponse<any, any> = await axios({
+                method: 'PUT',
+                url: `${SERVER_URL}/admin/gallery/${data.galleryId}`,
+                data: formData,
+                headers: {
+                    'Content-Type': 'multipart/form-data',
+                    Authorization: accessToken,
+                },
+            })
+            return { message: response.data.message }
+        },
+        [data, image],
+    )
+}
+
+export function deleteGallery(galleryId: number) {
+    /**
+     * 갤러리 삭제
+     *
+     * @param galleryId - 갤러리 id
+     * @returns 서버 응답 메시지
+     *
+     * @beta
+     * @todo
+     * 테스트
+     *
+     */
+
+    return axiosRequestHandler(
+        async (galleryId) => {
+            const response: AxiosResponse<any, any> = await axios({
+                method: 'DELETE',
+                url: `${SERVER_URL}/admin/gallery/${galleryId}`,
+                headers: {
+                    Authorization: accessToken,
+                },
+            })
+            return { message: response.data.message }
+        },
+        [galleryId],
     )
 }
