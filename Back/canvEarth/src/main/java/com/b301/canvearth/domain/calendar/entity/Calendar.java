ddb2cfb8f219fd5d4724da6b5f691e0370c789c5
임혜령 <hyegpfud@gmail.com>
2024-04-23 16:06:50 +0900

    fix(calendar): calendar entity 컬럼 수정

--- Back/canvEarth/src/main/java/com/b301/canvearth/domain/calendar/entity/Calendar.java
+++ Back/canvEarth/src/main/java/com/b301/canvearth/domain/calendar/entity/Calendar.java
@@ -3,9 +3,7 @@ package com.b301.canvearth.domain.calendar.entity;
 import io.hypersistence.utils.hibernate.type.array.ListArrayType;
 import io.hypersistence.utils.hibernate.type.array.StringArrayType;
 import jakarta.persistence.*;
-import lombok.Builder;
-import lombok.Cleanup;
-import lombok.Getter;
+import lombok.*;
 import org.hibernate.annotations.Type;
 
 import java.util.Date;
@@ -14,6 +12,7 @@ import java.util.List;
 @Entity
 @Getter
 @Table(name="Calendar")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Calendar {
 
     @Id
@@ -27,45 +26,18 @@ public class Calendar {
     @Column(name="title", nullable = false)
     private String title;
 
-    @Column(name="company")
-    private String company;
-
     @Column(name="start_date")
     private String startDate;
 
     @Column(name="end_date")
     private String endDate;
 
-    @Temporal(TemporalType.TIMESTAMP)
-    @Column(name="upload_date")
-    private Date uploadDate;
-
-    @Type(ListArrayType.class)
-    @Column(columnDefinition = "varchar[]")
-    private List<String> tag;
-
-    @Column(name="original_path")
-    private String originalPath;
-
-    @Column(name="thumbnail_path")
-    private String thumbnailPath;
-
-    @Column(name="watermark_path")
-    private String watermarkPath;
-
     @Builder
-    public Calendar(Long id, String userId, String title, String company, String startDate, String endDate, Date uploadDate,
-                    List<String> tag, String originalPath, String thumbnailPath, String watermarkPath) {
+    public Calendar(Long id, String userId, String title, String startDate, String endDate) {
         this.id = id;
         this.userId = userId;
         this.title = title;
-        this.company = company;
         this.startDate = startDate;
         this.endDate = endDate;
-        this.uploadDate = uploadDate;
-        this.tag = tag;
-        this.originalPath = originalPath;
-        this.thumbnailPath = thumbnailPath;
-        this.watermarkPath = watermarkPath;
     }
 }
