78f8be6b5209140b5a9c7f033fe92a3a47a745ba
박예원 <parkyw0321@gmail.com>
2024-04-24 13:02:55 +0900

    feat(gallery): gallery service 생성

--- Back/canvEarth/src/main/java/com/b301/canvearth/domain/gallery/service/GalleryService.java
+++ Back/canvEarth/src/main/java/com/b301/canvearth/domain/gallery/service/GalleryService.java
@@ -1,4 +1,23 @@
 package com.b301.canvearth.domain.gallery.service;
 
+
+import com.b301.canvearth.domain.gallery.entity.Gallery;
+import com.b301.canvearth.domain.gallery.repository.GalleryRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
 public class GalleryService {
+
+    private final GalleryRepository galleryRepository;
+
+    public List<Gallery> findAllGallery(){
+        log.info("======== Start galleryService.findALlGallery ==============");
+        return galleryRepository.findAll();
+    }
 }
