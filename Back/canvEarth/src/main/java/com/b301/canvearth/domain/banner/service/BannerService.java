e62ab5a444ad431f828d789e5c4b66c54d67af05
박예원 <parkyw0321@gmail.com>
2024-04-24 15:22:27 +0900

    feat(banner): banner Service 생성

--- Back/canvEarth/src/main/java/com/b301/canvearth/domain/banner/service/BannerService.java
+++ Back/canvEarth/src/main/java/com/b301/canvearth/domain/banner/service/BannerService.java
@@ -1,4 +1,25 @@
 package com.b301.canvearth.domain.banner.service;
 
+import com.b301.canvearth.domain.banner.entity.Banner;
+import com.b301.canvearth.domain.banner.repository.BannerRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
 public class BannerService {
+
+    private final BannerRepository bannerRepository;
+
+    public List<Banner> findAllBanner(){
+        log.info("======== START galleryService.findALlGallery ==============");
+        return bannerRepository.findAll(Sort.by(Sort.Direction.DESC, "bannerId"));
+    }
+
+
 }
