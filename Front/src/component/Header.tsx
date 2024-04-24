6131f0576e7e753c665778e4e98e6503b76d051b
남찬현 <chanhyun0821@gmail.com>
2024-04-24 14:30:32 +0900

    feat(layout): 헤더에 프로필 추가

--- Front/src/component/Header.tsx
+++ Front/src/component/Header.tsx
@@ -1,5 +1,6 @@
 import styles from './Header.module.scss'
 import Link from 'next/link'
+import Profile from '@/component/Profile'
 
 const Header: React.FC = () => {
     return (
@@ -7,6 +8,17 @@ const Header: React.FC = () => {
             <h2>
                 <Link href="/">CanvEarth</Link>
             </h2>
+            <div></div>
+            <div className={styles.profile}>
+                <Profile
+                    src="https://pbs.twimg.com/media/FxeXXAeaEAATIVE?format=jpg&name=900x900"
+                    size={40}
+                    border="2px solid balck"
+                    alt="profile image"
+                    radius={50}
+                />
+                <h3>sanyamg</h3>
+            </div>
         </header>
     )
 }
