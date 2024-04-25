3207787ed3d1aa7c95d49a5b41842bec4c85b9fd
김태희 <apples5060@naver.com>
2024-04-26 01:50:33 +0900

    fix(security): UserController 로직 분리
    1. UserController에서 실행되던 서비스 로직을 UserService로 이전

--- Back/canvEarth/src/main/java/com/b301/canvearth/domain/user/controller/UserController.java
+++ Back/canvEarth/src/main/java/com/b301/canvearth/domain/user/controller/UserController.java
@@ -1,11 +1,7 @@
 package com.b301.canvearth.domain.user.controller;
 
-import com.b301.canvearth.domain.authorization.service.RefreshService;
 import com.b301.canvearth.domain.user.dto.SignInDto;
 import com.b301.canvearth.domain.user.service.UserService;
-import com.b301.canvearth.global.util.JWTUtil;
-import io.jsonwebtoken.ExpiredJwtException;
-import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.http.HttpStatus;
@@ -18,93 +14,39 @@ public class UserController {
 
     private final UserService userService;
 
-    private final JWTUtil jwtUtil;
-
-    private final RefreshService refreshService;
-
-    public UserController(UserService userService, JWTUtil jwtUtil, RefreshService refreshService) {
+    public UserController(UserService userService) {
 
         this.userService = userService;
-        this.jwtUtil = jwtUtil;
-        this.refreshService = refreshService;
     }
 
     @PostMapping("/signin")
-    public String signIn(SignInDto signinDto) {
-        userService.signInProcess(signinDto);
-        // return 처리 필요함
-        return "ok";
+    public ResponseEntity<?> signIn(SignInDto signinDto) {
+
+        String result = userService.signInProcess(signinDto);
+        HttpStatus status;
+
+        if(result.equals("User save success")){
+            status = HttpStatus.CREATED;
+        }else{
+            status = HttpStatus.BAD_REQUEST;
+        }
+
+        return new ResponseEntity<>(result, status);
     }
 
     @PostMapping("/reissue")
     public ResponseEntity<?> reIssue(HttpServletRequest request, HttpServletResponse response) {
 
-        String refresh = null;
+        String result = userService.reIssueProcess(request, response);
+        HttpStatus status;
 
-        Cookie[] cookies = request.getCookies();
-        if(cookies != null) {
-
-            for (Cookie cookie : cookies) {
-
-                if (cookie.getName().equals("refresh")) {
-
-                    refresh = cookie.getValue();
-                }
-            }
+        if(result.equals("Cookie creation success")){
+            status = HttpStatus.CREATED;
+        }else{
+            status = HttpStatus.BAD_REQUEST;
         }
 
-        if (refresh == null) {
-
-            return new ResponseEntity<>("refresh token null", HttpStatus.BAD_REQUEST);
-        }
-
-        try {
-
-            jwtUtil.isExpired(refresh);
-
-        } catch (ExpiredJwtException e) {
-
-            return new ResponseEntity<>("refresh token expired", HttpStatus.BAD_REQUEST);
-        }
-
-        String category = jwtUtil.getCategory(refresh);
-
-        if (!category.equals("refresh")) {
-
-            return new ResponseEntity<>("refresh token invalid", HttpStatus.BAD_REQUEST);
-        }
-
-        String username = jwtUtil.getUsername(refresh);
-        String role = jwtUtil.getRole(refresh);
-
-        boolean isExist = refreshService.isRefreshTokenValid(username, refresh);
-
-        if(!isExist){
-
-            return new ResponseEntity<>("refresh token not exist", HttpStatus.BAD_REQUEST);
-        }
-
-        String newAccess = jwtUtil.createJwt("access", username, role, 600000L);
-        String newRefresh = jwtUtil.createJwt("refresh", username, role, 86400000L);
-
-        refreshService.deleteRefreshToken(username);
-        refreshService.saveRefreshToken(username, newRefresh, 86400000L);
-
-        response.setHeader("access", newAccess);
-        response.addCookie(createCookie(newRefresh));
-
-        return new ResponseEntity<>(HttpStatus.OK);
-    }
-
-    private Cookie createCookie(String value) {
-
-        Cookie cookie = new Cookie("refresh", value);
-        cookie.setMaxAge(24 * 60 * 60);
-        cookie.setSecure(true);
-        cookie.setPath("/");
-        cookie.setHttpOnly(true);
-
-        return cookie;
+        return new ResponseEntity<>(result, status);
     }
 
 }
--- Back/canvEarth/src/main/java/com/b301/canvearth/domain/user/service/UserService.java
+++ Back/canvEarth/src/main/java/com/b301/canvearth/domain/user/service/UserService.java
@@ -1,8 +1,14 @@
 package com.b301.canvearth.domain.user.service;
 
+import com.b301.canvearth.domain.authorization.service.RefreshService;
 import com.b301.canvearth.domain.user.dto.SignInDto;
 import com.b301.canvearth.domain.user.entity.User;
 import com.b301.canvearth.domain.user.repository.UserRepository;
+import com.b301.canvearth.global.util.JWTUtil;
+import io.jsonwebtoken.ExpiredJwtException;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -10,24 +16,36 @@ import org.springframework.stereotype.Service;
 public class UserService {
 
     final private UserRepository userRepository;
+
     final private BCryptPasswordEncoder bCryptPasswordEncoder;
 
-    public UserService(UserRepository userRepository, BCryptPasswordEncoder bCryptPasswordEncoder) {
+    private final JWTUtil jwtUtil;
+
+    private final RefreshService refreshService;
+
+    public UserService(UserRepository userRepository, BCryptPasswordEncoder bCryptPasswordEncoder,
+                       JWTUtil jwtUtil, RefreshService refreshService) {
         this.userRepository = userRepository;
         this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+        this.jwtUtil = jwtUtil;
+        this.refreshService = refreshService;
     }
 
 
-    public void signInProcess(SignInDto signinDto) {
+    public String signInProcess(SignInDto signinDto) {
+
+        // 1. 회원중복 조회
         String username = signinDto.getUsername();
         String password = signinDto.getPassword();
 
+        // 1-1. Username => ID
         boolean isExist = userRepository.existsById(username);
 
         if(isExist){
-            return;
+            return "User exists";
         }
 
+        // 회원 등록
         User data = new User();
         data.setId(username);
         data.setUserName(username);
@@ -35,6 +53,70 @@ public class UserService {
         data.setRole("ROLE_ADMIN");
 
         userRepository.save(data);
+
+        return "User save success";
+    }
+
+    public String reIssueProcess(HttpServletRequest request, HttpServletResponse response){
+
+        // 1. 쿠키에서 refresh 토큰 검색
+        String refresh = null;
+        Cookie[] cookies = request.getCookies();
+        if(cookies != null) {
+            for (Cookie cookie : cookies) {
+                if (cookie.getName().equals("refresh")) {
+                    refresh = cookie.getValue();
+                }
+            }
+        }
+
+        if (refresh == null) {
+            return "refresh token null";
+        }
+
+        // 2. 토큰 카테고리가 refresh 인지 대조
+        String category = jwtUtil.getCategory(refresh);
+        if (!category.equals("refresh")) {
+            return "refresh token invalid";
+        }
+
+        // 3. refresh 토큰 유효기간 검증
+        try {
+            jwtUtil.isExpired(refresh);
+        } catch (ExpiredJwtException e) {
+            return "refresh token expired";
+        }
+
+        // 4. Redis 에서 refresh 토큰 2차 검증
+        String username = jwtUtil.getUsername(refresh);
+        String role = jwtUtil.getRole(refresh);
+        boolean isExist = refreshService.isRefreshTokenValid(username, refresh);
+        if(!isExist){
+            return "refresh token not exist";
+        }
+
+        // 5. access, refresh 토큰 재발급
+        String newAccess = jwtUtil.createJwt("access", username, role, 600000L);
+        String newRefresh = jwtUtil.createJwt("refresh", username, role, 86400000L);
+
+        refreshService.deleteRefreshToken(username);
+        refreshService.saveRefreshToken(username, newRefresh, 86400000L);
+
+        response.setHeader("access", newAccess);
+        response.addCookie(createCookie(newRefresh));
+
+        return "Cookie creation success";
+    }
+
+    private Cookie createCookie(String value) {
+        // 1. 쿠키 생성
+        Cookie cookie = new Cookie("refresh", value);
+        cookie.setMaxAge(24 * 60 * 60);
+        cookie.setSecure(true);
+        cookie.setPath("/");
+        cookie.setHttpOnly(true);
+
+        return cookie;
     }
 
 }
