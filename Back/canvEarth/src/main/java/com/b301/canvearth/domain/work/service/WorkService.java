6b5f792dc093eecca9e95f1d570fd11bafb21e05
임혜령 <hyegpfud@gmail.com>
2024-04-26 14:53:10 +0900

    feat(work): S3 업로드 시 등록 경로 하나의 메소드로 분리 및 work 수정 서비스 추가

--- Back/canvEarth/src/main/java/com/b301/canvearth/domain/work/service/WorkService.java
+++ Back/canvEarth/src/main/java/com/b301/canvearth/domain/work/service/WorkService.java
@@ -5,6 +5,7 @@ import com.amazonaws.SdkClientException;
 import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.b301.canvearth.domain.admin.dto.WorkRequestPostDto;
+import com.b301.canvearth.domain.admin.dto.WorkRequestPutDto;
 import com.b301.canvearth.domain.s3.service.S3Service;
 import com.b301.canvearth.domain.work.entity.Work;
 import com.b301.canvearth.domain.work.repository.WorkRepository;
@@ -17,7 +18,9 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.UUID;
 
 @Service
@@ -32,29 +35,16 @@ public class WorkService {
     private String bucket;
 
     public Work insertWork(MultipartFile image, WorkRequestPostDto requestPostDto) {
-        log.info("===== [WorkService] s3Insert start =====");
+        log.info("===== [WorkService] insertWork start =====");
 
-        UUID uuid = UUID.randomUUID();
-        // originalPath
-        String originalPath = s3Service.uploadImage(image, uuid, "original");
+        Map<String, String> paths = uploadS3AndGetPath(image);
 
-        // thumbnailPath
-        String thumbnailPath = s3Service.uploadImage(image, uuid, "thumbnail");
-
-        // watermarkPath
-        String watermarkPath = s3Service.uploadImage(image, uuid, "watermark");
-
-        if(originalPath == null) {
-            log.error("originalPath가 비어있습니다.");
-            return null;
-            // exception 던지기
-        }
-
-        // TODO: 썸네일, 워터마크 해줘야함.
         Work work = Work.builder().userId(requestPostDto.getUserId()).company(requestPostDto.getCompany())
                 .title(requestPostDto.getTitle()).startDate(requestPostDto.getStartDate())
                 .endDate(requestPostDto.getEndDate()).tags(requestPostDto.getTags())
-                .originalPath(originalPath).thumbnailPath(thumbnailPath).watermarkPath(watermarkPath)
+                .originalPath(paths.get("originalPath"))
+                .thumbnailPath(paths.get("thumbnailPath"))
+                .watermarkPath(paths.get("watermarkPath"))
                 .build();
 
         // DB에 저장
@@ -65,5 +55,55 @@ public class WorkService {
 
     public List<Work> getWorkList() {
         return workRepository.findAll();
+    }
+
+    public Work modifyWork(Long workId, MultipartFile image, WorkRequestPutDto requestPutDto) {
+        log.info("===== [WorkService] modifyWork start =====");
+
+        // 이미지 변경
+        // 이미지가 빈 경우 이미지는 수정하지 않음.
+        // 이미지가 비지 않은 경우 받은 이미지로 S3에 이미지 변경 후 DB에도 반영
+        boolean changeImage = false;
+        Map<String, String> paths = new HashMap<>();
+        if(!image.isEmpty()) {
+            paths = uploadS3AndGetPath(image);
+            changeImage = true;
+        }
+
+        Work changeWork = workRepository.findById(workId)
+                .orElseThrow(() -> new IllegalArgumentException("Calendar not found with id: " + workId));
+
+        changeWork.setCompany(requestPutDto.getCompany());
+        changeWork.setTitle(requestPutDto.getTitle());
+        changeWork.setStartDate(requestPutDto.getStartDate());
+        changeWork.setEndDate(requestPutDto.getEndDate());
+        changeWork.setTags(requestPutDto.getTags());
+
+        if(changeImage) {
+            changeWork.setOriginalPath(paths.get("originalPath"));
+            changeWork.setThumbnailPath(paths.get("thumbnailPath"));
+            changeWork.setWatermarkPath(paths.get("watermarkPath"));
+        }
+
+        return workRepository.save(changeWork);
+    }
+
+    public Map<String, String> uploadS3AndGetPath(MultipartFile image) {
+        Map<String, String> paths = new HashMap<>();
+        UUID uuid = UUID.randomUUID();
+        // originalPath
+        String originalPath = s3Service.uploadImage(image, uuid, "original");
+        paths.put("originalPath", originalPath);
+
+        // TODO: 썸네일, 워터마크 해줘야함.
+        // thumbnailPath
+        String thumbnailPath = s3Service.uploadImage(image, uuid, "thumbnail");
+        paths.put("thumbnailPath", thumbnailPath);
+
+        // watermarkPath
+        String watermarkPath = s3Service.uploadImage(image, uuid, "watermark");
+        paths.put("watermarkPath", watermarkPath);
+
+        return paths;
     }
 }
