8a450ae4228c9a43d3c7f476ad3cb596c9da2991
임혜령 <hyegpfud@gmail.com>
2024-04-30 15:22:43 +0900

    feat(gallery): 갤러리 등록 Dto 추가

---
+++ Back/canvEarth/src/main/java/com/b301/canvearth/domain/admin/dto/GalleryRequestPostDto.java
@@ -0,0 +1,36 @@
+package com.b301.canvearth.domain.admin.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.ToString;
+
+import java.util.List;
+
+@Getter
+@ToString
+public class GalleryRequestPostDto {
+
+    private final String title;
+    private final String createDate;
+    private final List<String> tags;
+
+    @Builder
+    public GalleryRequestPostDto(String title, String createDate, List<String> tags) {
+        this.title = title;
+        this.createDate = createDate;
+        this.tags = tags;
+    }
+
+    public String isValid() {
+        String isValid = "valid";
+
+        if(title == null || title.isEmpty()) {
+            isValid = "title";
+        } else if(createDate == null || createDate.isEmpty()) {
+            isValid = "startDate";
+        } else if(tags == null || tags.isEmpty()) {
+            isValid = "tags";
+        }
+        return isValid;
+    }
+}
