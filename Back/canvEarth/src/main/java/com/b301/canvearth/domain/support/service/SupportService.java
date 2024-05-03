697336c9e836b474d9f5ed9c6ff0128f51607db6
임혜령 <hyegpfud@gmail.com>
2024-05-03 16:11:16 +0900

    feat(support): support 수정 서비스 추가

--- Back/canvEarth/src/main/java/com/b301/canvearth/domain/support/service/SupportService.java
+++ Back/canvEarth/src/main/java/com/b301/canvearth/domain/support/service/SupportService.java
@@ -1,14 +1,18 @@
 package com.b301.canvearth.domain.support.service;
 
 import com.b301.canvearth.domain.admin.dto.SupportRequestPostDto;
+import com.b301.canvearth.domain.admin.dto.SupportRequestPutDto;
 import com.b301.canvearth.domain.s3.service.S3Service;
 import com.b301.canvearth.domain.support.entity.Support;
 import com.b301.canvearth.domain.support.repository.SupportRepository;
+import com.b301.canvearth.domain.work.entity.Work;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.UUID;
 
 @Service
@@ -25,8 +29,6 @@ public class SupportService {
         UUID uuid = UUID.randomUUID();
         String supportThumbnailPath = s3Service.uploadImage(image, uuid, "support");
 
-//        SupportLinkList supportLink = requestPostDto.getSupportLink();
-
         Support support = Support.builder().supportThumbnail(supportThumbnailPath).title(requestPostDto.getTitle())
                 .supportLink(requestPostDto.getSupportLink())
                 .content(requestPostDto.getContent())
@@ -35,5 +37,31 @@ public class SupportService {
         Support insertSupport = supportRepository.save(support);
         log.info("success insertSupport: {}", insertSupport);
         return insertSupport;
+    }
+
+    public Support modifySupport(Long supportId, MultipartFile image, SupportRequestPutDto requestPutDto) {
+        log.info("===== [SupportService] modifySupport start =====");
+
+        boolean changeImage = false;
+        String path = "";
+        if(image != null && !image.isEmpty()) {
+            UUID uuid = UUID.randomUUID();
+            path = s3Service.uploadImage(image, uuid, "support");
+            changeImage = true;
+        }
+
+        Support changeSupport = supportRepository.findById(supportId)
+                .orElseThrow(()-> new IllegalArgumentException("해당 supportId로 데이터를 찾을 수 없습니다. supportId: " + supportId));
+
+        changeSupport.setTitle(requestPutDto.getTitle());
+        changeSupport.setContent(requestPutDto.getContent());
+        changeSupport.setSupportLink(requestPutDto.getSupportLink());
+
+        if(changeImage) {
+            s3Service.deleteImage(changeSupport.getSupportThumbnail());
+            changeSupport.setSupportThumbnail(path);
+        }
+
+        return supportRepository.save(changeSupport);
     }
 }
