79980f4907a3a39d63747c27efdb52452925d82f
김태희 <apples5060@naver.com>
2024-05-03 09:29:34 +0900

    fix(security): swagger response 데이터 입력

--- Back/canvEarth/src/main/java/com/b301/canvearth/domain/user/controller/UserController.java
+++ Back/canvEarth/src/main/java/com/b301/canvearth/domain/user/controller/UserController.java
@@ -3,6 +3,7 @@ package com.b301.canvearth.domain.user.controller;
 import com.b301.canvearth.domain.user.dto.SignInDto;
 import com.b301.canvearth.domain.user.service.UserService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -27,6 +28,9 @@ public class UserController {
     private final UserService userService;
 
     @Operation(summary = "REQ-USER-03", description = "회원가입")
+    @ApiResponse(responseCode = "201", description = "회원가입 성공")
+    @ApiResponse(responseCode = "409", description = "중복된 ID 입니다")
+    @ApiResponse(responseCode = "409", description = "중복된 닉네임 입니다")
     @PostMapping("/signin")
     public ResponseEntity<Object> signIn(SignInDto signinDto) {
 
@@ -46,6 +50,11 @@ public class UserController {
     }
 
     @Operation(summary = "REQ-USER-06", description = "JWT 재발급")
+    @ApiResponse(responseCode = "201", description = "refresh 토큰 재발행 성공")
+    @ApiResponse(responseCode = "401", description = "refresh 토큰이 존재하지 않습니다")
+    @ApiResponse(responseCode = "401", description = "만료된 refresh 토큰입니다")
+    @ApiResponse(responseCode = "401", description = "잘못된 refresh 토큰입니다")
+    @ApiResponse(responseCode = "401", description = "사용하지 않는 refresh 토큰입니다")
     @PostMapping("/reissue")
     public ResponseEntity<Object> reIssue(HttpServletRequest request, HttpServletResponse response) {
 
