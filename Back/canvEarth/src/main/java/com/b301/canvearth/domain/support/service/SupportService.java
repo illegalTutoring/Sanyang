b919247f53d9bae03d7b078b67f501e92f8152b1
임혜령 <hyegpfud@gmail.com>
2024-05-03 14:56:26 +0900

    feat(support): support 등록 서비스 기능 추가

--- Back/canvEarth/src/main/java/com/b301/canvearth/domain/support/service/SupportService.java
+++ Back/canvEarth/src/main/java/com/b301/canvearth/domain/support/service/SupportService.java
@@ -1,4 +1,39 @@
 package com.b301.canvearth.domain.support.service;
 
+import com.b301.canvearth.domain.admin.dto.SupportRequestPostDto;
+import com.b301.canvearth.domain.s3.service.S3Service;
+import com.b301.canvearth.domain.support.entity.Support;
+import com.b301.canvearth.domain.support.repository.SupportRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
 public class SupportService {
+
+    private final SupportRepository supportRepository;
+    private final S3Service s3Service;
+
+    public Support insertSupport(MultipartFile image, SupportRequestPostDto requestPostDto) {
+        log.info("===== [SupportService] insertSupport start =====");
+
+        UUID uuid = UUID.randomUUID();
+        String supportThumbnailPath = s3Service.uploadImage(image, uuid, "support");
+
+//        SupportLinkList supportLink = requestPostDto.getSupportLink();
+
+        Support support = Support.builder().supportThumbnail(supportThumbnailPath).title(requestPostDto.getTitle())
+                .supportLink(requestPostDto.getSupportLink())
+                .content(requestPostDto.getContent())
+                .build();
+
+        Support insertSupport = supportRepository.save(support);
+        log.info("success insertSupport: {}", insertSupport);
+        return insertSupport;
+    }
 }
