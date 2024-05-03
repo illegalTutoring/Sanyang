fc1c6c997673cedebe04481c97c6ce69c4f22961
임혜령 <hyegpfud@gmail.com>
2024-05-03 16:10:45 +0900

    feat(support): support 수정 request, response dto 추가

---
+++ Back/canvEarth/src/main/java/com/b301/canvearth/domain/admin/dto/SupportRequestPutDto.java
@@ -0,0 +1,34 @@
+package com.b301.canvearth.domain.admin.dto;
+
+import com.b301.canvearth.domain.support.entity.SupportLink;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.ToString;
+
+import java.util.List;
+
+@Getter
+@ToString
+public class SupportRequestPutDto {
+
+    private final String title;
+    private final List<SupportLink> supportLink;
+    private final String content;
+
+    @Builder
+    public SupportRequestPutDto(String title, List<SupportLink> supportLink, String content) {
+        this.title = title;
+        this.supportLink = supportLink;
+        this.content = content;
+    }
+
+    public String isValid() {
+        String isValid = "valid";
+
+        if(title == null || title.isEmpty()) {
+            isValid = "title";
+        }
+
+        return isValid;
+    }
+}
