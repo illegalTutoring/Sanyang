bb652c29c05b89be063825dc019f3f34f92ef937
김태희 <apples5060@naver.com>
2024-05-03 09:30:08 +0900

    feat(security): User 엔티티 username 으로 검색 메소드 구현

--- Back/canvEarth/src/main/java/com/b301/canvearth/domain/user/entity/User.java
+++ Back/canvEarth/src/main/java/com/b301/canvearth/domain/user/entity/User.java
@@ -3,10 +3,12 @@ package com.b301.canvearth.domain.user.entity;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 @Entity
 @Getter
 @Setter
+@ToString
 @Table(name="Member")
 public class User {
 
