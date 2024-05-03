bfe3b55d744fa2ba504607bf9b77083ff73e5298
임혜령 <hyegpfud@gmail.com>
2024-05-03 14:56:56 +0900

    feat(support): support 등록 controller에 추가(insertSupport)

---
+++ Back/canvEarth/src/main/java/com/b301/canvearth/domain/admin/controller/AdminSupportController.java
@@ -0,0 +1,70 @@
+package com.b301.canvearth.domain.admin.controller;
+
+import com.b301.canvearth.domain.admin.dto.SupportRequestPostDto;
+import com.b301.canvearth.domain.support.entity.Support;
+import com.b301.canvearth.domain.support.service.SupportService;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/admin/support")
+@Tag(name = "admin support", description = "ADMIN SUPPORT API")
+@Slf4j
+public class AdminSupportController {
+
+    private static final String MESSAGE = "message";
+
+    private final SupportService supportService;
+
+    @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Object> registSupport(@RequestPart MultipartFile image,
+                                                @RequestPart("data") SupportRequestPostDto requestPostDto) {
+        log.info("===== [AdminSupportController] registSupport start =====");
+        log.info("[requestData]: {}", requestPostDto);
+
+        Map<String, Object> responseBody = new HashMap<>();
+
+        // image라는 form-data는 받았지만 빈 파일일 경우
+        if(image.isEmpty()) {
+            String errorMessage = "image가 비어있습니다.";
+            log.error(errorMessage);
+            responseBody.put(MESSAGE, errorMessage);
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(responseBody);
+        }
+
+
+        String isValidSupportDto = requestPostDto.isValid();
+        if(!isValidSupportDto.equals("valid")) {
+            String errorMessage = String.format("입력한 값에 문제가 있습니다. [%s] 데이터를 확인해주세요.", isValidSupportDto);
+            log.error(errorMessage);
+            responseBody.put(MESSAGE, errorMessage);
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(responseBody);
+        }
+
+        Support insertSupport = supportService.insertSupport(image, requestPostDto);
+        if(insertSupport == null) {
+            log.error("insertSupport 비어있음.");
+            responseBody.put(MESSAGE, "후원 등록을 실패하였습니다.");
+            // TODO: exception 바꿔야 함
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(responseBody);
+        }
+
+        responseBody.put(MESSAGE, "후원 등록을 완료하였습니다.");
+        log.info("[responseData] {}", responseBody);
+        return ResponseEntity.status(HttpStatus.OK).body(responseBody);
+
+    }
+}
