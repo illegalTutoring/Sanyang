82af776753a9768b0fb7e4e083df2427954449de
임혜령 <hyegpfud@gmail.com>
2024-05-03 14:55:37 +0900

    feat(support): support entity builder 추가 및 supportLink 타입 수정

--- Back/canvEarth/src/main/java/com/b301/canvearth/domain/support/entity/Support.java
+++ Back/canvEarth/src/main/java/com/b301/canvearth/domain/support/entity/Support.java
@@ -2,14 +2,12 @@ package com.b301.canvearth.domain.support.entity;
 
 import io.hypersistence.utils.hibernate.type.json.JsonBinaryType;
 import jakarta.persistence.*;
-import lombok.AccessLevel;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.ToString;
+import lombok.*;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.Type;
 
 import java.util.Date;
+import java.util.List;
 
 @Entity
 @Getter
@@ -38,8 +36,19 @@ public class Support {
 //    @Column(name="support_link", length=500)
     @Type(JsonBinaryType.class)
     @Column(name="support_link", columnDefinition = "jsonb")
-    private SupportLink supportLink;
+    private List<SupportLink> supportLink;
 
     @Column(name="content")
     private String content;
+
+    @Builder
+    public Support(Long id, String supportThumbnail, String title, Date uploadDate,
+                   List<SupportLink> supportLink, String content) {
+        this.id = id;
+        this.supportThumbnail = supportThumbnail;
+        this.title = title;
+        this.uploadDate = uploadDate;
+        this.supportLink = supportLink;
+        this.content = content;
+    }
 }
