d01a5f13170b231ccaeb319970037733ee2a1d83
박예원 <parkyw0321@gmail.com>
2024-05-07 10:23:51 +0900

    refactor(notice): 불필요한 import 삭제, request response 분리

--- Back/canvEarth/src/main/java/com/b301/canvearth/domain/notice/entity/Notice.java
+++ Back/canvEarth/src/main/java/com/b301/canvearth/domain/notice/entity/Notice.java
@@ -10,7 +10,6 @@ import org.hibernate.annotations.DynamicUpdate;
 import org.hibernate.annotations.UpdateTimestamp;
 import org.springframework.format.annotation.DateTimeFormat;
 
-import java.sql.Timestamp;
 import java.util.Date;
 
 @Entity
