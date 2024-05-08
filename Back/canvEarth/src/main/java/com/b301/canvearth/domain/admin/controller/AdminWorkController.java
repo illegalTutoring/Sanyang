df77e80bcf771d184956eee57afcc275eb0c6214
임혜령 <hyegpfud@gmail.com>
2024-05-08 10:12:54 +0900

    feat(work): swagger operation 추가, 입력 문제 시 custom exception 발생

--- Back/canvEarth/src/main/java/com/b301/canvearth/domain/admin/controller/AdminWorkController.java
+++ Back/canvEarth/src/main/java/com/b301/canvearth/domain/admin/controller/AdminWorkController.java
@@ -6,6 +6,9 @@ import com.b301.canvearth.domain.admin.dto.request.WorkRequestPutDto;
 import com.b301.canvearth.domain.admin.dto.response.WorkResponsePutDto;
 import com.b301.canvearth.domain.work.entity.Work;
 import com.b301.canvearth.domain.work.service.WorkService;
+import com.b301.canvearth.global.error.CustomException;
+import com.b301.canvearth.global.error.ErrorCode;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -29,6 +32,7 @@ public class AdminWorkController {
 
     private final WorkService workService;
 
+    @Operation(summary = "REQ-ADMIN-01", description = "외주 등록")
     @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Object> registWork(@RequestPart MultipartFile image,
                                              @RequestPart("data") WorkRequestPostDto requestPostDto){
@@ -50,23 +54,18 @@ public class AdminWorkController {
         if(!isValidWorkDto.equals("valid")) {
             String errorMessage = String.format("입력한 값에 문제가 있습니다. [%s] 데이터를 확인해주세요.", isValidWorkDto);
             log.error(errorMessage);
-            responseBody.put(MESSAGE, errorMessage);
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(responseBody);
+            throw new CustomException(ErrorCode.NO_REQUIRE_ARUGUMENT, errorMessage);
         }
 
         Work insertWork = workService.insertWork(image, requestPostDto);
-        if(insertWork == null) {
-            log.error("insertWork 비어있음.");
-            responseBody.put(MESSAGE, "외주 작품 등록을 실패하였습니다.");
-            // TODO: exception 바꿔야 함
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(responseBody);
-        }
+        log.info("insertWork: {}", insertWork);
 
         responseBody.put(MESSAGE, "외주 작품 등록을 완료하였습니다.");
         log.info("[responseData] {}", responseBody);
         return ResponseEntity.status(HttpStatus.OK).body(responseBody);
     }
 
+    @Operation(summary = "REQ-ADMIN-01", description = "외주 수정")
     @PutMapping("/{workId}")
     public ResponseEntity<Object> modifyWork(@PathVariable("workId") Long workId,
                                              @RequestPart(value="image", required = false) MultipartFile image,
@@ -81,8 +80,7 @@ public class AdminWorkController {
         if(!isValidWorkDto.equals("valid")) {
             String errorMessage = String.format("입력한 값에 문제가 있습니다. [%s] 데이터를 확인해주세요.", isValidWorkDto);
             log.error(errorMessage);
-            responseBody.put(MESSAGE, errorMessage);
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(responseBody);
+            throw new CustomException(ErrorCode.NO_REQUIRE_ARUGUMENT, errorMessage);
         }
 
         Work modifyWork = workService.modifyWork(workId, image, requestPutDto);
@@ -98,6 +96,7 @@ public class AdminWorkController {
         return ResponseEntity.status(HttpStatus.OK).body(responseBody);
     }
 
+    @Operation(summary = "REQ-ADMIN-01", description = "외주 삭제")
     @DeleteMapping("/{workId}")
     public ResponseEntity<Object> deleteWork(@PathVariable("workId") Long workId) {
         log.info("===== [AdminWorkController] deleteWork start =====");
