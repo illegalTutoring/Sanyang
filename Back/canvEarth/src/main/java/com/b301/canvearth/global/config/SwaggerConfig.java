b9e69d85c8f6b6f0e04b05dadf07e0d035a1f747
김태희 <apples5060@naver.com>
2024-05-09 10:21:02 +0900

    feat(security): SwaggerConfig 설정을 통한 로그인 JWT 설정, Login Filter 로직 API 테스트 환경 추가

---
+++ Back/canvEarth/src/main/java/com/b301/canvearth/global/config/SwaggerConfig.java
@@ -0,0 +1,89 @@
+package com.b301.canvearth.global.config;
+
+import io.swagger.v3.oas.models.*;
+import io.swagger.v3.oas.models.media.*;
+import io.swagger.v3.oas.models.parameters.RequestBody;
+import io.swagger.v3.oas.models.responses.ApiResponse;
+import io.swagger.v3.oas.models.responses.ApiResponses;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .components(
+                        new Components()
+                                .addSecuritySchemes("accessToken", new SecurityScheme()
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("accessToken")
+                                )
+                                .addSecuritySchemes("refreshToken", new SecurityScheme()
+                                        .type(SecurityScheme.Type.APIKEY)
+                                        .in(SecurityScheme.In.COOKIE)
+                                        .name("refreshToken")
+                                )
+                )
+                .paths(new Paths()
+                        .addPathItem("/api/user/login", new PathItem()
+                                .post(new Operation()
+                                        .tags(List.of("user"))
+                                        .operationId("login")
+                                        .summary("REQ-USER-01")
+                                        .description("로그인(Access, Refresh 토큰 발급)")
+                                        .requestBody(new RequestBody()
+                                                .content(new Content()
+                                                        .addMediaType("multipart/form-data", new MediaType()
+                                                                .schema(new ObjectSchema()
+                                                                        .addProperty("username", new StringSchema()
+                                                                                .description("User's username")
+                                                                        )
+                                                                        .addProperty("password", new StringSchema()
+                                                                                .description("User's password")
+                                                                        )
+                                                                )
+                                                        )
+                                                )
+                                        )
+                                        .responses(new ApiResponses()
+                                                .addApiResponse("200", new ApiResponse()
+                                                        .description("Successful login")
+                                                        .content(new Content()
+                                                                .addMediaType("application/json", new MediaType()
+                                                                        .schema(new Schema<>()
+                                                                                .addProperty("message", new StringSchema())
+                                                                                .addProperty("username", new StringSchema())
+                                                                                .addProperty("refreshToken", new StringSchema()) // 추가
+                                                                        )
+                                                                )
+                                                        )
+                                                )
+                                                .addApiResponse("401", new ApiResponse()
+                                                        .description("Unauthorized")
+                                                        .content(new Content()
+                                                                .addMediaType("application/json", new MediaType()
+                                                                        .schema(new Schema<>()
+                                                                                .addProperty("message", new StringSchema())
+                                                                        )
+                                                                )
+                                                        )
+                                                )
+                                        )
+//                                        .security(new ArrayList<>(
+//                                                Arrays.asList(
+//                                                        new SecurityRequirement().addList("bearerAuth"),
+//                                                        new SecurityRequirement().addList("refreshToken")
+//                                                )
+//                                        ))
+                                )
+                        )
+                );
+    }
+
+}
