5c9daf2156bc16499cf023467146de79fe8ee18b
임혜령 <hyegpfud@gmail.com>
2024-05-13 12:53:05 +0900

    fix(gallery): gallery 등록 시 userId를 받지 않도록 변경

--- Back/canvEarth/src/main/java/com/b301/canvearth/domain/admin/dto/request/GalleryRequestPostDto.java
+++ Back/canvEarth/src/main/java/com/b301/canvearth/domain/admin/dto/request/GalleryRequestPostDto.java
@@ -10,14 +10,12 @@ import java.util.List;
 @ToString
 public class GalleryRequestPostDto {
 
-    private final String userId;
     private final String title;
     private final String createDate;
     private final List<String> tags;
 
     @Builder
-    public GalleryRequestPostDto(String userId, String title, String createDate, List<String> tags) {
-        this.userId = userId;
+    public GalleryRequestPostDto(String title, String createDate, List<String> tags) {
         this.title = title;
         this.createDate = createDate;
         this.tags = tags;
@@ -26,9 +24,7 @@ public class GalleryRequestPostDto {
     public String isValid() {
         String isValid = "valid";
 
-        if(userId == null || userId.isEmpty()) {
-            isValid = "userId";
-        } else if(title == null || title.isEmpty()) {
+        if(title == null || title.isEmpty()) {
             isValid = "title";
         } else if(createDate == null || createDate.isEmpty()) {
             isValid = "startDate";
